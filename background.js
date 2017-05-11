03964895b55a9795e4452fc11891094c00ee20a4
don <donfreiday@email.com>
2017-05-11 10:01:03 -0400

    Added code to background.js to click Load to card buttons automatically on giantfoodstores.com.

--- background.js
+++ background.js
@@ -2,7 +2,9 @@ function onWebNav(details) {
     if (details.frameId === 0) {
         // Top-level frame
         chrome.pageAction.show(details.tabId);
-    }
+        var buttons = document.getElementsByClassName('button-alternate load-to-card'); ​
+        for(var i = 0; i <= buttons.length; i++)
+            buttons[i].click();
 }
 var filter = {
     url: [{
