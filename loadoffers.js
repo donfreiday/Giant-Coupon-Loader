463d5c9df82c43d17435b882e025803147e19eea
don <donfreiday@email.com>
2017-08-02 12:53:07 -0400

    Update button class name to reflect changes on Giant's website.

--- loadoffers.js
+++ loadoffers.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   setTimeout(function(){
-    var buttons = document.getElementsByClassName('button-alternate load-to-card');
+    var buttons = document.getElementsByClassName('c-coupon__button a-button -red -grows -full-width js-load-to-card js-coremetrics-location');
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].click();
     }
